c77adb3da73c8f7e0bf0a6e2d69f2144d4ac4c7c
Jane Anyanwu <janeanyanwu668@gmail.com>
2026-09-22 23:57:57 +0100

    feat: configure Flask with environment variables

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,4 +8,7 @@ def home():
     return "Hello from my Dockerized Flask application!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    host = os.getenv("HOST", "0.0.0.0")
+    port = int(os.getenv("PORT", "5000"))
+
+    app.run(host=host, port=port)
